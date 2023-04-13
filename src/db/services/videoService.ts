70681e1679dccf585b6e7f7d66a5e52193efbad3
Karen Torres <ktorresno@gmail.com>
2023-04-13 16:42:01 -0600

    Service to expose data access to our APIs

---
+++ src/db/services/videoService.ts
@@ -0,0 +1,27 @@
+import * as videoDal from '../dal/video';
+import {GetAllVideosFilters} from '../dal/types';
+import {VideoInput, VideoOuput} from '../models/Video';
+
+export const create = (payload: VideoInput): Promise<VideoOuput> => {
+    return videoDal.create(payload);
+}
+
+export const update = (id: number, payload: Partial<VideoInput>): Promise<VideoOuput> => {
+    return videoDal.update(id, payload);
+}
+
+export const getById = (id: number): Promise<VideoOuput> => {
+    return videoDal.getById(id);
+}
+
+export const deleteById = (id: number): Promise<boolean> => {
+    return videoDal.deleteById(id);
+}
+
+export const getAll = (filters: GetAllVideosFilters): Promise<VideoOuput[]> => {
+    return videoDal.getAll(filters);
+}
+
+export const validateByUrl = (url: string): Promise<boolean> => {
+    return videoDal.validateByUrl(url);
+}
