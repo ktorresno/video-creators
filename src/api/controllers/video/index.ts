61b16eb745f1795ad961620f4627c63fa70fa089
Karen Torres <ktorresno@gmail.com>
2023-04-13 16:47:12 -0600

    Controller used to hadle video routes requirements.

---
+++ src/api/controllers/video/index.ts
@@ -0,0 +1,25 @@
+import * as service from '../../../db/services/videoService';
+import { CreateVideoDTO, UpdateVideoDTO, FilterVideosDTO } from '../../dto/video.dto';
+import { Video } from '../../interfaces';
+import * as mapper from './mapper';
+
+export const create = async(payload: CreateVideoDTO): Promise<Video> => {
+    return mapper.toVideo(await service.create(payload))
+}
+
+export const update = async (id: number, payload: UpdateVideoDTO): Promise<Video> => {
+    return mapper.toVideo(await service.update(id, payload))
+}
+
+export const getById = async (id: number): Promise<Video> => {
+    return mapper.toVideo(await service.getById(id))
+}
+
+export const deleteById = async(id: number): Promise<Boolean> => {
+    const isDeleted = await service.deleteById(id)
+    return isDeleted
+}
+
+export const getAll = async(filters: FilterVideosDTO): Promise<Video[]> => {
+    return (await service.getAll(filters)).map(mapper.toVideo)
+}
