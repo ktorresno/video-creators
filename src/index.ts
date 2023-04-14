91769b6ccd592f936ca6c35b39da619586700fc4
Karen Torres <ktorresno@gmail.com>
2023-04-14 11:01:50 -0600

    Alloeing PATCH

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ export const get = () => {
   // CORS config Middleware
   app.use((req: Request, res: Response, next: NextFunction) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
     /*
       res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
       if (req.method === "OPTIONS") {
