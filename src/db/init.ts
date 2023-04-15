86cc1e8ed4a2233872edba7cefac1afe0d88dc92
Karen Torres <ktorresno@gmail.com>
2023-04-15 17:41:06 -0600

    Sequalize Sync for Users

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config';
-import { Video } from './models';
+import { Video, User } from './models';
 
 const isDev = process.env.NODE_ENV === 'development';
 const isTest = process.env.NODE_ENV === 'test';
 
 const dbInit = async() => {
     await Video.sync({ alter: isDev || isTest });
+    await User.sync({ alter: isDev || isTest });
 }
 
 export default dbInit;
