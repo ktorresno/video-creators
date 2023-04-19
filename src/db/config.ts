24b894f4355ecec07799b3323bfe2e48f2595abe
Karen Torres <ktorresno@gmail.com>
2023-04-19 10:21:13 -0600

    Reconfiguration to work with sequealize seeder

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,23 +1,39 @@
 import 'dotenv/config';
-import { Dialect, Sequelize } from 'sequelize';
+import { Dialect } from 'sequelize';
+import { Sequelize } from 'sequelize-typescript';
 
 const isTest = process.env.NODE_ENV === 'test';
 
 const dbName = isTest ? process.env.TEST_PG_DB as string : process.env.PG_DB as string;
-const dbUser = process.env.PG_USER as string;
+const dbUser = process.env.PG_USER;
 const dbHost = process.env.PG_HOST;
 const dbPassword = process.env.PG_PASSWORD;
-const dbDriver = process.env.PG_DRIVER as Dialect;
+const dbDriver = process.env.PG_DRIVER || 'postgres';
+const dbPort = 5432;
+
+export const development = {
+    database: dbName,
+    username: dbUser as string,
+    password: dbPassword,
+    // Defaults for Postgres
+    host: dbHost,
+    port: dbPort,
+    dialect: dbDriver as Dialect,
+    logging: false
+};
 
 const sequelizeConnection = new Sequelize(
-    dbName,
-    dbUser,
-    dbPassword,
-    {
-        host: dbHost,
-        dialect: dbDriver,
-        logging: false
-    }
+   development.database,
+   development.username,
+   development.password,
+   {
+        host: development.host,
+        dialect: development.dialect,
+        logging: development.logging,
+        dialectOptions: {
+            multipleStatements: true
+        }
+   }
 );
 
 export default sequelizeConnection;
