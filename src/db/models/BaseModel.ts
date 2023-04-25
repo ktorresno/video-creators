ee8346d95281095799668d0b240ca2a72955689a
Karen Torres <ktorresno@gmail.com>
2023-04-25 17:44:02 -0600

    Common class used in our modules as template

---
+++ src/db/models/BaseModel.ts
@@ -0,0 +1,21 @@
+import {
+    Model, Table, Column, PrimaryKey, AutoIncrement, CreatedAt,
+    DeletedAt, UpdatedAt, 
+} from 'sequelize-typescript';
+
+@Table
+export class BaseModel<TModelAttributes extends {} = any, TCreationAttributes extends {} = TModelAttributes>
+            extends Model<TModelAttributes, TCreationAttributes> {
+    // timestamps!
+    @CreatedAt
+    @Column
+    public readonly createdAt!: Date;
+    
+    @UpdatedAt
+    @Column
+    public readonly updatedAt!: Date;
+    
+    @DeletedAt
+    @Column
+    public readonly deletedAt!: Date;
+}
