c0ebdf51e638a13d38cca23d6302cbf114a5ada6
Karen Torres <ktorresno@gmail.com>
2023-04-25 17:53:33 -0600

    New seed files which declares data structure to feed Videos and Users entities

--- src/db/seeders/videos.ts
+++ src/db/seeders/videos.ts
@@ -1,12 +1,10 @@
 'use strict';
 
-/** @type {import('sequelize-cli')} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    return await queryInterface.bulkInsert('Videos', [{
+export const videosData = [{
       title: 'Test video title 1',
       description: 'Test video title 1 description',
       url: 'https://www.tiktok.com/@misodope/video/718826992223',
+      userId: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     },
@@ -14,6 +12,7 @@ module.exports = {
       title: 'Test video title 2',
       description: 'Test video title 2 description',
       url: 'https://www.tiktok.com/@lalalee/video/45826993434',
+      userId: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     },
@@ -21,6 +20,7 @@ module.exports = {
       title: 'Test video title 3',
       description: 'Test video title 3 description',
       url: 'https://www.tiktok.com/@thujie/video/34718826877',
+      userId: 2,
       createdAt: new Date(),
       updatedAt: new Date()
     },
@@ -28,15 +28,23 @@ module.exports = {
       title: 'Test video title 4',
       description: 'Test video title 4 description',
       url: 'https://www.tiktok.com/@4thujie/video/831860900',
+      userId: 3,
       createdAt: new Date(),
       updatedAt: new Date()
-    }], {});
-  },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Commands to revert seed here.
-     */
-    await queryInterface.bulkDelete('Videos', null, {});
-  }
-};
+    },
+    {
+      title: 'Test video title 5',
+      description: 'Test video title 5 description',
+      url: 'https://www.tiktok.com/@4thujie/video/83122860900',
+      userId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      title: 'Test video title 6',
+      description: 'Test video title 6 description',
+      url: 'https://www.tiktok.com/@4thujie/video/83186090066',
+      userId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }];
