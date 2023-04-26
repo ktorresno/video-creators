08d96300e97598eee0dd6b08fc61bd6fa9b07aaa
Karen Torres <ktorresno@gmail.com>
2023-04-26 17:44:59 -0600

    Data structure to insert into entity LikedVideos

--- src/db/seeders/videos.ts
+++ src/db/seeders/videos.ts
@@ -1,50 +1,63 @@
 'use strict';
 
-export const videosData = [{
-      title: 'Test video title 1',
-      description: 'Test video title 1 description',
-      url: 'https://www.tiktok.com/@misodope/video/718826992223',
-      userId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      title: 'Test video title 2',
-      description: 'Test video title 2 description',
-      url: 'https://www.tiktok.com/@lalalee/video/45826993434',
-      userId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      title: 'Test video title 3',
-      description: 'Test video title 3 description',
-      url: 'https://www.tiktok.com/@thujie/video/34718826877',
-      userId: 2,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      title: 'Test video title 4',
-      description: 'Test video title 4 description',
-      url: 'https://www.tiktok.com/@4thujie/video/831860900',
-      userId: 3,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      title: 'Test video title 5',
-      description: 'Test video title 5 description',
-      url: 'https://www.tiktok.com/@4thujie/video/83122860900',
-      userId: 4,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      title: 'Test video title 6',
-      description: 'Test video title 6 description',
-      url: 'https://www.tiktok.com/@4thujie/video/83186090066',
-      userId: 3,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }];
+export const videosData = [
+  {
+    title: 'Test video title 1',
+    description: 'Test video title 1 description',
+    url: 'https://www.tiktok.com/@misodope/video/718826992223',
+    userId: 1,
+  }, {
+    title: 'Test video title 2',
+    description: 'Test video title 2 description',
+    url: 'https://www.tiktok.com/@lalalee/video/45826993434',
+    userId: 1,
+  }, {
+    title: 'Test video title 3',
+    description: 'Test video title 3 description',
+    url: 'https://www.tiktok.com/@thujie/video/34718826877',
+    userId: 2,
+  }, {
+    title: 'Test video title 4',
+    description: 'Test video title 4 description',
+    url: 'https://www.tiktok.com/@4thujie/video/831860900',
+    userId: 3,
+  }, {
+    title: 'Test video title 5',
+    description: 'Test video title 5 description',
+    url: 'https://www.tiktok.com/@4thujie/video/83122860900',
+    userId: 4,
+  }, {
+    title: 'Test video title 6',
+    description: 'Test video title 6 description',
+    url: 'https://www.tiktok.com/@4thujie/video/83186090066',
+    userId: 3,
+  }
+];
+
+export const likedVideosData = [
+  {
+    creatorId: 1,
+    videoId: 1
+  }, {
+    creatorId: 3,
+    videoId: 1
+  }, {
+    creatorId: 2,
+    videoId: 2
+  }, {
+    creatorId: 3,
+    videoId: 2
+  }, {
+    creatorId: 3,
+    videoId: 3
+  },{
+    creatorId: 4,
+    videoId: 3
+  },{
+    creatorId: 1,
+    videoId: 4
+  },{
+    creatorId: 2,
+    videoId: 4
+  }
+];
