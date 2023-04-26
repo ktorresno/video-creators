da0d911fcae505f4f6f02cdd594cc204b0746446
Karen Torres <ktorresno@gmail.com>
2023-04-26 17:47:51 -0600

    Using promise flow to control the order in how the entities are populated.

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,27 +1,41 @@
 import sequelizeConnection from './config';
-import { Video, User, getModels } from './models';
-import { usersData } from './seeders/users'
-import { videosData } from './seeders/videos'
+import { Video, User, getModels, LikedVideo, FollowCreator } from './models';
+import { usersData, followCreatorsData } from './seeders/users'
+import { videosData, likedVideosData } from './seeders/videos'
 
 export const dbInit = async() => {
   await sequelizeConnection.addModels(getModels());
   sequelizeConnection.sync({ force: true })
-    .then(() => populateTables());
+    .then(async() => {
+      await createUsers();
+      await createVideos();
+    })
+    .then(async() => {
+      await createLikedVideos();
+      await createFollowers();
+    });
 };
 
-const populateTables = () => {
-   createUsers();
-   createVideos();
-};
-
-const createUsers = () => {
-  usersData.map(async(user) => {
+const createUsers = async() => {
+  await usersData.map(async(user) => {
     await User.create(user)
   });
 };
 
-const createVideos = () => {
-  videosData.map(async(video) => {
-    await Video.create(video)
+const createVideos = async() => {
+    await videosData.map(async(video) => {
+      await Video.create(video)
   });
 };
+
+const createLikedVideos = async() => {
+  await likedVideosData.map(async(likeItem) => {
+    await LikedVideo.create(likeItem)
+  });
+};
+
+const createFollowers = async() => {
+  await followCreatorsData.map(async(following) => {
+    await FollowCreator.create(following)
+  });
+}
